97435aea14ffe549b9333fdd2c0de9910670351b
Shane Kaestner <shane.kaestner@yahoo.com>
2021-05-28 16:48:01 -0700

    minor refactor to allow me to create different classes for tutorials and call them from the same main method.

---
+++ CSharp Tutorial/CSharp Tutorial/Fibonacci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Tutorial
+{
+    class Fibonacci
+    {
+        public static void forLoopFib(List<int> list)
+        {
+            var fibonacciNumbers = list;
+
+            for (int i = 0; i < 18; i++)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+            }
+
+
+
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
+        }
+    }
+}
--- CSharp Tutorial/CSharp Tutorial/Program.cs
+++ CSharp Tutorial/CSharp Tutorial/Program.cs
@@ -7,20 +7,8 @@ namespace CSharp_Tutorial
     {
         static void Main(string[] args)
         {
-            var fibonacciNumbers = new List<int> { 1, 1 };
-
-            for (int i = 0; i < 18; i++)
-            {
-                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
-                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
-
-                fibonacciNumbers.Add(previous + previous2);
-            }
-
-
-
-            foreach (var item in fibonacciNumbers)
-                Console.WriteLine(item);
+            List<int> start = new List<int> { 1, 1 };
+            Fibonacci.forLoopFib(start);
         }
     }
 }
