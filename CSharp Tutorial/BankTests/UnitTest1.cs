48644756b5001b4a25f7f30e008a3ccc7637bec9
Shane Kaestner <shane.kaestner@yahoo.com>
2021-06-02 00:12:36 -0700

    added test for a valid withdrawal

--- CSharp Tutorial/BankTests/UnitTest1.cs
+++ CSharp Tutorial/BankTests/UnitTest1.cs
@@ -7,13 +7,13 @@ namespace BankTests
     public class UnitTests
     {
         [Fact]
-        public void Test1()
+        public void TestTrue()
         {
             Assert.True(true);
         }
 
         [Fact]
-        public void Test2()
+        public void CannotOverdraw()
         {
             var account = new BankAccount("Warbucks", 6173);
 
@@ -23,7 +23,14 @@ namespace BankTests
 
         }
         [Fact]
-        public void Test3()
+        public void CanWithdrawValid()
+        {
+            var account = new BankAccount("Warbucks", 6173);
+            account.MakeWithdrawal(1000, DateTime.Now, "Attempt to withdraw");
+            Assert.True(account.Balance == 5173);
+        }
+        [Fact]
+        public void CannotStartNegativeAccount()
         {
             // Test that the initial balances must be positive.
             BankAccount invalidAccount;
