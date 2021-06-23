2d300f87e16be6910d5f75924a39004835a29de3
Shane Kaestner <shane.kaestner@yahoo.com>
2021-06-23 00:03:18 -0700

    working on sollers assignment, and I have option # 1 and 6 working

---
+++ CSharp Tutorial/StudentApp/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentApp
+{
+    class Student
+    {
+        int id { get; }
+
+        public string studentName { get; set; }
+
+        public int studentAge { get; set; }
+
+        public string studentEmail { get; set; }
+
+        private static int idSeed = 1;
+
+        public Student(string name, int age, string email)
+        {
+            this.studentName = name;
+            this.studentAge = age;
+            this.studentEmail = email;
+            this.id = idSeed;
+            idSeed++;
+        }
+
+        public static void getStudents(List<Student> list)
+        {
+            if (list.Count > 0)
+            {
+                foreach(Student item in list)
+                {
+                    Console.WriteLine($"Student's name is {item.studentName}, ID #{item.id}, " +
+                        $"student's age is {item.studentAge} and their email is {item.studentEmail}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No records");
+            }
+        }
+    }
+}
---
+++ CSharp Tutorial/StudentApp/StudentMain.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentApp
+{
+    class StudentMain
+    {
+        static void Main(string[] args)
+        {
+            int userNumber = 1;
+            var students = new List<Student>();
+            Student james = new Student("james", 28, "a@b.c");
+            students.Add(james);
+            do
+            {
+                Console.WriteLine("please enter a number between 1 and 6");
+                string userInput = Console.ReadLine();
+                userNumber = Convert.ToInt32(userInput);
+                switch(userNumber)
+                {
+                    case 1:
+                        Student.getStudents(students);
+                        continue;
+                    case 2:
+                        Console.WriteLine("hello 1");
+                        continue;
+                    case 3:
+                        Console.WriteLine("hello 1");
+                        continue;
+                    case 4:
+                        Console.WriteLine("hello 1");
+                        continue;
+                    case 5:
+                        Console.WriteLine("hello 1");
+                        continue;
+                    case 6:
+                        Console.WriteLine("We're done here");
+                        break;
+
+                    default:
+                        Console.WriteLine($"{userNumber} is not a valid input, must be between 1 and 6");
+                        continue;
+                }
+
+            } while (userNumber != 6);
+            
+            //Console.WriteLine(james.studentName);
+            //james.studentName = "Josh";
+            //Console.WriteLine(james.ToString());
+            Console.ReadKey();
+        }
+    }
+}
