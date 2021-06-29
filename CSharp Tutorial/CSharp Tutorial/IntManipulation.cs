634f9141ba46b590c360f7eaa81351b0113eea3d
Shane Kaestner <shane.kaestner@yahoo.com>
2021-06-29 13:24:39 -0700

    update

--- CSharp Tutorial/CSharp Tutorial/IntManipulation.cs
+++ CSharp Tutorial/CSharp Tutorial/IntManipulation.cs
@@ -1,18 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CSharp_Tutorial
 {
     class IntManipulation
     {
-
         public static void MultiplyNumber()
         {
             Console.WriteLine("Please enter a number");
             int num = 0;
             string input;
-            bool check = false;
             input = Console.ReadLine();
             if (int.TryParse(input, out num))
             {
@@ -26,7 +22,5 @@ namespace CSharp_Tutorial
                 Console.WriteLine($"Sorry but {input} is not in a correct format");
             }
         }
-
-
     }
 }
